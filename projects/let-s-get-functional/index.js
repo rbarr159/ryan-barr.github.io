75cb7a7380c2d618920d2f52dfec276b9a30ecf0
rbarr159 <barr.ryanw@gmail.com>
2022-01-21 04:00:00 +0000

    finished Lets Get Functional

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -3,7 +3,7 @@
 'use strict';
 
 var customers = require('./data/customers.json');
-var _ = require('underbar');
+var _ = require('lodown-rbarr159');
 
 /**
  * 1. Import your lodown module using the require() method,
@@ -16,7 +16,7 @@ var _ = require('underbar');
  *
  * 4. To test your work, run the following command in your terminal:
  *
- *    npm start --prefix ./ryan-barr.github.io/projects/let-s-get-functional
+ *    npm start --prefix ./projects/let-s-get-functional
  *
  *    IMPORTANT: Make sure you replace <YOUR_GITHUB_FOLDER with your actual github folder name that is in your workspace.
  */
@@ -30,29 +30,127 @@ var maleCount = function(array) {
 };
 
 var femaleCount = function(array) {
-    var numFemales = _.reduce(array, function(females, customer, index) {
+    var females = _.reduce(array, function(acc, customer) {
         if (customer.gender === 'female') {
-            numFemales += 1
-        }
-    })
-    return numFemales
+            return acc = acc + 1;
+        } else {return acc;}
+    }, 0)
+    return females
 }
 
-var oldestCustomer;
+var oldestCustomer = function(array) {
+    let oldestUser = array[0];
+    for(let i = 0; i < array.length; i++) {
+        if (array[i].age > oldestUser.age) {
+            oldestUser = array[i]
+        }
+    } return oldestUser.name;
+}
+;
 
-var youngestCustomer;
+var youngestCustomer = function(array) {
+    let youngestUser = array[0];
+    for(let i = 0; i < array.length; i++) {
+        if (array[i].age < youngestUser.age) {
+            youngestUser = array[i]
+        }
+    } return youngestUser.name;
+}
+;
 
-var averageBalance;
+var averageBalance = function(array) {    
+    var sum = _.reduce(array, function(acc, customer) {
+        let str = customer.balance.slice(1).replace(/,/g,'')
+        return acc += +str;
+}, 0)
 
-var firstLetterCount;
+return sum / array.length;
+}
+;
 
-var friendFirstLetterCount;
+var firstLetterCount = function (arr, letter) {
+    var count = _.reduce(arr, function (acc, user) {
+        if (user.name[0].toUpperCase() === letter.toUpperCase()) {
+            return acc = acc + 1;
+        } else { return acc; }
+    }, 0)
+    return count
+};
 
-var friendsCount;
+var friendFirstLetterCount = function (arr, customer, letter) {
+    let obj = {};
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].name === customer) {
+            Object.assign(obj, arr[i])
+        }
+    }
+    var count = _.reduce(obj.friends, function (acc, friend) {
+        if (friend.name.charAt(0).toUpperCase() === letter.toUpperCase()) {
+            return acc = acc + 1;
+        } else { return acc; }
+    }, 0)
+    return count
+};
 
-var topThreeTags;
+var friendsCount = function (array, name) {
+    var count = _.filter(array, function (customer) {
+        for (let i = 0; i < customer.friends.length; i++) {
+            if (Object.values(customer.friends[i]).includes(name)) {
+                return true;
+            }
+        }
 
-var genderCount;
+    })
+    let names = [];
+    for (let i = 0; i < count.length; i++) {
+        names.push(count[i].name)
+    }
+    return names
+} 
+
+var topThreeTags = function (array) {
+    let tags = [];
+    let output = [];
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].tags.length; j++) {
+            tags.push(array[i].tags[j])
+        }
+    }
+    let tagCount = {}
+    for (let i = 0; i < tags.length; i++) {
+        if (Object.keys(tagCount).includes(tags[i])) {
+            tagCount[tags[i]] += 1;
+        } else {
+            tagCount[tags[i]] = 1;
+        }
+    } let vals = Object.values(tagCount);
+    let max = Math.max(...vals)
+    for (var key in tagCount) {
+        if (tagCount[key] === max) {
+            output.push(key);
+        }
+}
+        return output;
+}
+
+var genderCount = function(array) {
+    var males = _.filter(array, function(customer) {
+        return customer.gender === 'male';
+    }) 
+    var females = _.filter(array, function(customer) {
+        return customer.gender === 'female';
+    }) 
+    var nb = _.filter(array, function(customer) {
+        return customer.gender === 'non-binary';
+    }) 
+    return {
+        male: males.length,
+        female: females.length,
+        'non-binary': nb.length,
+        
+    }
+}
+;
 
 //////////////////////////////////////////////////////////////////////
 // DON'T REMOVE THIS CODE ////////////////////////////////////////////
